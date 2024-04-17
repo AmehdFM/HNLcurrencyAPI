7d92528bb0b1a014a549ba099db7318ee3cb12ba
AmehdFM <94817400+AmehdFM@users.noreply.github.com>
2024-04-16 20:54:24 -0600

    agregar variabes de entorno

--- database/db.py
+++ database/db.py
@@ -2,8 +2,8 @@ import os
 from supabase import create_client, Client
 
 # Obtener la URL y la clave de acceso de Supabase desde las variables de entorno
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = os.environ.get("supabaseURL")
+key: str = os.environ.get("supabaseKEY")
 
 # Crear cliente de Supabase
 supabase: Client = create_client(url, key)
